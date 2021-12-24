d88ac8746ec50ff80bd6b105d3a6292167d62f99
tksgi <23550705+tksgi@users.noreply.github.com>
2021-12-25 03:08:00 +0900

    1.5章 写経 tcp_clientを実装

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 #[macro_use]
 extern crate log;
 
-// mod tcp_client;
+mod tcp_client;
 mod tcp_server;
 // mod udp_client;
 // mod udp_server;
@@ -27,6 +27,7 @@ fn main() {
             }
             "client" => {
                 // TODO: TCPクライアントの呼び出し
+                tcp_client::connect(address).unwrap_or_else(|e| error!("{}", e));
             }
             _ => {
                 missing_role();
