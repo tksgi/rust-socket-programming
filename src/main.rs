7f998382153edc45052f53db80ca9c376e5820c9
tksgi <23550705+tksgi@users.noreply.github.com>
2021-12-25 02:52:07 +0900

    comment out not implemented module

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use std::env;
 #[macro_use]
 extern crate log;
 
-mod tcp_client;
+// mod tcp_client;
 mod tcp_server;
-mod udp_client;
-mod udp_server;
+// mod udp_client;
+// mod udp_server;
 
 fn main() {
     env::set_var("RUST_LOG", "debug");
