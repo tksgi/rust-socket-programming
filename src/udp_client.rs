b666a6367a94cb4d9777bceb714a5018fc090351
tksgi <23550705+tksgi@users.noreply.github.com>
2021-12-25 03:40:32 +0900

    try to send too big data

--- src/udp_client.rs
+++ src/udp_client.rs
@@ -3,9 +3,10 @@ use std::{io, str};
 
 pub fn communicate(address: &str) -> Result<(), failure::Error> {
     let socket = UdpSocket::bind("127.0.0.1:0")?;
-    loop {
-        let mut input = String::new();
-        io::stdin().read_line(&mut input)?;
+    // loop {
+        // let mut input = String::new();
+        // io::stdin().read_line(&mut input)?;
+        let input = "a".to_string().repeat(70000);
         socket.send_to(input.as_bytes(), address)?;
 
         let mut buffer = [0u8; 1024];
@@ -14,5 +15,6 @@ pub fn communicate(address: &str) -> Result<(), failure::Error> {
             "{}",
             str::from_utf8(&buffer).expect("failed to convert to String")
         );
-    }
+    // }
+    return Ok(());
 }
