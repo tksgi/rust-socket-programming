756a3ffd8158bcad57eb98935b6b7030b63fad10
tksgi <23550705+tksgi@users.noreply.github.com>
2021-12-27 01:30:51 +0900

    2.2.2写経 main.rsの実装

--- src/main.rs
+++ src/main.rs
@@ -34,4 +34,130 @@ fn main() {
         .into_iter()
         .find(|iface| iface.name == *interface_name)
         .expect("Failed to get interface");
+
+    /* [1]: データリンクのチャンネルを取得 */
+    let (_tx, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type"),
+        Err(e) => panic!("Failed to create datalink channel {}", e),
+    };
+
+    loop {
+        match rx.next() {
+            Ok(frame) => {
+                // 受信データからイーサネットフレームの構築
+                let frame = EthernetPacket::new(frame).unwrap();
+                match frame.get_ethertype() {
+                    EtherTypes::Ipv4 => {
+                        ipv4_handler(&frame);
+                    }
+                    EtherTypes::Ipv6 => {
+                        ipv6_handler(&frame);
+                    }
+                    _ => {
+                        info!("Not an IPv4 or IPv6 Packet");
+                    }
+                }
+            }
+            Err(e) => {
+                error!("Failed to read: {}", e);
+            }
+        }
+    }
 }
+
+/**
+* Ipv4パケットを構築し次のレイヤのハンドラを呼び出す
+*/
+fn ipv4_handler(ethernet: &EthernetPacket) {
+    if let Some(packet) = Ipv4Packet::new(ethernet.payload()) {
+        match packet.get_next_level_protocol() {
+            IpNextHeaderProtocols::Tcp => {
+                tcp_handler(&packet);
+            }
+            IpNextHeaderProtocols::Udp => {
+                udp_handler(&packet);
+            }
+            _ => {
+                info!("Not a TCP or UDP packet");
+            }
+        }
+    }
+}
+
+/**
+* Ipv6パケットを構築し次のレイヤのハンドラを呼び出す
+*/
+fn ipv6_handler(ethernet: &EthernetPacket) {
+    if let Some(packet) = Ipv6Packet::new(ethernet.payload()) {
+        match packet.get_next_handler() {
+            IpNextHeaderProtocols::Tcp => {
+                tcp_handler(&packet);
+            }
+            IpNextHeaderProtocols::Ucp => {
+                Ucp_handler(&packet);
+            }
+            _ => {
+                info!("Not a TCP or UDP packet");
+            }
+        }
+    }
+}
+
+/**
+* TCPパケットを構築する
+*/
+fn tcp_handler(packet: &GettableEndPoints) {
+    let tcp = TcpPacket::new(packet.get_payload());
+    if let Some(tcp) = tcp {
+        print_packet_info(packet, &tcp, "TCP");
+    }
+}
+
+/**
+* UDPパケットを構築する
+*/
+fn udp_handler(packet: &GettableEndPoints) {
+    let udp = UdpPacket::new(packet.get_payload());
+    if let Some(udp) = udp {
+        print_packet_info(packet, &udp, "UDP");
+    }
+}
+
+/**
+* アプリケーション層のデータをバイナリで表示する。
+*/
+fn print_packet_info(l3: &GettableEndPoints, l4: &GettableEndPoints, proto: &str) {
+    println!(
+        "Captured a {} packet from {}|{} to {}|{}\n",
+        proto,
+        l3.get_source(),
+        l4.get_source(),
+        l3.get_destination()
+    );
+    let payload = l4.get_payload();
+    let len = payload.len();
+    // ペイロードの表示
+    // 指定した定数幅で表示を行う
+    for i in 0..len {
+        print!("{:<02X} ", payload[i]);
+        if i % WIDTH == WIDTH - 1 || i == len -1 {
+            for _j in 0..WIDTH - 1 - (i % (WIDTH)) {
+                print!(" ");
+            }
+            print!("| ");
+            for j in i - i % WIDTH..=i {
+                if payload[j].is_ascii_alphabetic() {
+                    print("{}", payload[j] as char);
+                } else {
+                    // 非ascii文字は.で表示
+                    print!(".");
+                }
+            }
+            println!();
+        }
+    }
+    println!("{}", "=".repead(WIDTH * 3));
+    println!();
+}
+
