64152f8eca326a49aa54294a72d34d7e96ee0e15
tksgi <23550705+tksgi@users.noreply.github.com>
2021-12-28 17:13:39 +0900

    3.4.1写経 enumを定義

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,22 @@ extern crate log;
 
 const TCP_SIZE: usize = 20;
 
+struct PacketInfo {
+    my_ipaddr: Ipv4Addr,
+    target_ipaddr: Ipv4Addr,
+    my_port: u16,
+    maximum_port: u16,
+    scan_type: ScanType,
+}
+
+#[derive(Copy, Clone)]
+enum ScanType {
+    Syn = TcpFlags::SYN as isize,
+    Fin = TcpFlags::FIN as isize,
+    Xmas = (TcpFlags::FIN | TcpFlags::URG | TcpFlags::PSH) as isize,
+    Null = 0,
+
+}
+
 fn main() {
-    println!("Hello, world!");
 }
