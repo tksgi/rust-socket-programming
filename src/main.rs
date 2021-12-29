ee4c34c6975d8866aecf20a01811c935ac455767
tksgi <23550705+tksgi@users.noreply.github.com>
2021-12-29 18:35:52 +0900

    3.4.2写経 argsと.envの扱いを実装

--- src/main.rs
+++ src/main.rs
@@ -30,4 +30,39 @@ enum ScanType {
 }
 
 fn main() {
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        error!("Bad number of arguments. [ipaddr] [scantype]");
+        process::exit(1);
+    }
+
+    let packet_info = {
+        let contsnts = fs::read_to_string(".env").expect("Failed to read env file");
+        let lines: Vec<_> = contents.split('\n').collect();
+        let mut map = HashMap::new();
+        for line in lines {
+            let elm: Vec<_> = line.split('=').map(str::trim).collect();
+            if elm.len() == 2 {
+                map.insert(elm[0], elm[1]);
+            }
+        }
+        PacketInfo {
+            my_ipaddr: map["MY_IPADDR"].parse().expect("invalid ipaddr"),
+            target_ipaddr: args[1].parse().expect("invalid target ipaddr"),
+            my_port: map["MY_PORT"].parse().expect("invalid port number"),
+            maximum_port: map["MAXIMUM_PORT_NUM"].parse().expect("invalid maximum port num"),
+            scan_type: match args[2].as_str() {
+                "sS" => ScanType::Syn,
+                "sF" => ScanType::Fin,
+                "sX" => ScanType::Xmas,
+                "sN" => ScanType::Null,
+                _ => {
+                    error!("Undefined scan method, only accept [sS|sF|sN|sX].");
+                    process::exit(1);
+                }
+            },
+        }
+    };
 }
