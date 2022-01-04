050d63537b7714a7ee2a1c3d5d114efe812961d9
tksgi <23550705+tksgi@users.noreply.github.com>
2022-01-04 23:32:54 +0900

    4.5.1 Webserver structの定義&初期化処理実装

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,29 @@ const SERVER: Token = Token(0);
 const WEBROOT: &str = "/webroot";
 
 
+struct WebServer {
+    listening_socket: TcpListener,
+    connections: HashMap<usize, TcpStream>, // サーバーに接続されているクライアントをハッシュで管理
+    next_connection_id: usize,
+}
+
+impl WebServer {
+    /**
+    * サーバーの初期化
+    */
+    fn new(addr: &str) -> Result<Self, failure:Error> {
+        let address = addr.parse()?;
+        let listening_socket = TcpListener::bind(&address)?;
+        Ok(WebServer {
+            listening_socket,
+            connections: HashMap::new(),
+            next_connection_id,
+            })
+    }
+
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
