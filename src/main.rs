07c07fb1092dea529752894c80ba45a940efb003
tksgi <23550705+tksgi@users.noreply.github.com>
2022-01-05 00:46:52 +0900

    4.5.2 サーバー初期化、接続要求時のイベント発生ロジックを実装

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,8 @@ use std::{env, process, str};
 #[macro_use]
 extern crate log;
 
-
 const SERVER: Token = Token(0);
 const WEBROOT: &str = "/webroot";
-
 
 struct WebServer {
     listening_socket: TcpListener,
@@ -21,20 +19,72 @@ struct WebServer {
 
 impl WebServer {
     /**
-    * サーバーの初期化
-    */
-    fn new(addr: &str) -> Result<Self, failure:Error> {
+     * サーバーの初期化
+     */
+    fn new(addr: &str) -> Result<Self, failure::Error> {
         let address = addr.parse()?;
         let listening_socket = TcpListener::bind(&address)?;
         Ok(WebServer {
             listening_socket,
             connections: HashMap::new(),
             next_connection_id: 1,
-            })
+        })
     }
 
-}
+    /**
+     * サーバーを起動
+     */
+    fn run(&mut self) -> Result<(), failure::Error> {
+        let poll = Poll::new()?;
+        // サーバーソケットの状態を監視対象に登録
+        poll.register(
+            &self.listening_socket,
+            SERVER,
+            Ready::readable(),
+            PollOpt::level(),
+        )?;
 
+        // イベントキュー
+        let mut events = Events::with_capacity(1024);
+        // HTTPのレスポンス用バッファ
+        let mut response = Vec::new();
+
+        loop {
+            // 現在のスレッドをブロックしてイベントを待つ
+            match poll.poll(&mut events, None) {
+                Ok(_) => {}
+                Err(e) => {
+                    error!("{ }", e);
+                    continue;
+                }
+            }
+            for event in &events {
+                match event.token() {
+                    SERVER => {
+                        // リスニングソケットの読み込み準備完了イベントが発生
+                        let (stream, remote) = match self.listening_socket.accept() {
+                            Ok(t) => t,
+                            Err(e) => {
+                                error!("{ }", e);
+                                continue;
+                            }
+                        };
+                        debug!("Connection from { }", &remote);
+                        // 接続済みソケットを監視対象に登録
+                        self.register_connection(&poll, stream)
+                            .unwrap_or_else(|e| error!("{ }", e));
+                    }
+
+                    Token(conn_id) => {
+                        // 接続済みソケットでイベント発生
+                        self.http_handler(conn_id, event, &poll, &mut response)
+                            .unwrap_or_else(|e| error!("{  }", e));
+                    }
+                }
+            }
+        }
+    }
+}
 
 fn main() {
     println!("Hello, world!");
