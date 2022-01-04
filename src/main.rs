c1b05ab20ec674e5d706b9d504e9deebf726cb66
tksgi <23550705+tksgi@users.noreply.github.com>
2022-01-04 23:28:07 +0900

    add use and Constants paragraph

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate mio;
+extern crate regex;
+
+use std::net::SocketAddr;
+use std::{ env, str, fs };
+use std::io::{ Error, Read, BufReader, Write };
+use std::collections::HashMap;
+
+use mio::*;
+use mio::tcp::{ TcpListener, TcpStream };
+use regex::Regex;
+
+
+const SERVER: Token = Token(0);
+const WEBROOT: &str = "/webroot";
+
+
 fn main() {
     println!("Hello, world!");
 }
