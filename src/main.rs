c91697c872c846b57b1f4268f141ea773257e570
tksgi <23550705+tksgi@users.noreply.github.com>
2022-01-09 19:23:43 +0900

    4.5.4 HTTP レスポンスの作成処理を実装

--- src/main.rs
+++ src/main.rs
@@ -109,10 +109,19 @@ impl WebServer {
     }
 
     /**
-    * 接続済みソケットで発生したイベントのハンドラ
-    */
-    fn http_handler(&mut self, conn_id: usize, event: Event, poll: &Poll, response: &mut Vec<u8>,) -> Result<(), failure::Error>{
-        let stream = self.connections.get_mut(&conn_id).ok_or_else(|| failure::err_msg("Failed to get connection."))?;
+     * 接続済みソケットで発生したイベントのハンドラ
+     */
+    fn http_handler(
+        &mut self,
+        conn_id: usize,
+        event: Event,
+        poll: &Poll,
+        response: &mut Vec<u8>,
+    ) -> Result<(), failure::Error> {
+        let stream = self
+            .connections
+            .get_mut(&conn_id)
+            .ok_or_else(|| failure::err_msg("Failed to get connection."))?;
         if event.readiness().is_readable() {
             // ソケットから読み込み可能
             debug!("readable conn_id: {}", conn_id);
@@ -120,7 +129,7 @@ impl WebServer {
             let nbytes = stream.read(&mut buffer)?;
 
             if nbytes != 0 {
-                *respose = make_response(&buffer[..nbytes])?;
+                *response = make_response(&buffer[..nbytes])?;
                 // 書き込み操作の可否を監視対象に入れる
                 poll.reregister(stream, Token(conn_id), Ready::writable(), PollOpt::edge())?;
             } else {
@@ -137,7 +146,73 @@ impl WebServer {
         } else {
             Err(failure::err_msg("Undefined event."))
         }
+    }
+}
 
+/**
+* レスポンスをバイト列で作成して返す
+*/
+fn make_response(buffer: &[u8]) -> Result<Vec<u8>, failure::Error> {
+    // リクエストラインをパースする
+    let http_pattern = Regex::new(r"(.*) (.*) HTTP/1.([0-1])\r\n.*")?;
+    let captures = match http_pattern.captures(str::from_utf8(buffer)?) {
+        Some(cap) => cap,
+        None => {
+            // 不正なリクエスト
+            return create_msg_from_code(400, None);
+        }
+    };
+    let method = captures[1].to_string();
+    let path = format!(
+        "{}{}{}",
+        env::current_dir()?.display(),
+        WEBROOT,
+        &captures[2]
+    );
+    let _version = captures[3].to_string();
+
+    if method == "GET" {
+        let file = match File::open(path) {
+            Ok(file) => file,
+            Err(_) => {
+                // パーミッションエラー等もここに含まれるが、簡略化のためnot foundを返す
+                return create_msg_from_code(404, None);
+            }
+        };
+        let mut reader = BufReader::new(file);
+        let mut buf = Vec::new();
+        reader.read_to_end(&mut buf)?;
+        create_msg_from_code(200, Some(buf))
+    } else {
+        // サポートしていないHTTPメソッド
+        create_msg_from_code(501, None)
+    }
+}
+
+/**
+* HTTPステータスコードからメッセージを生成する
+*/
+fn create_msg_from_code(status_code: u16, msg: Option<Vec<u8>>) -> Result<Vec<u8>, failure::Error> {
+    match status_code {
+        200 => {
+            let mut header = "HTTP/1.0 200 OK\r\n
+                Server: mio webserver\r\n\r\n"
+                .to_string()
+                .into_bytes();
+            if let Some(mut msg) = msg {
+                header.append(&mut msg);
+            }
+            Ok(header)
+        }
+        400 => Ok("HTTP/1.0 400 Bad Request\r\n
+            Server: mio webserver\r\n\r\n"
+            .to_string()
+            .into_bytes()),
+        501 => Ok("HTTP/1.0 501 Not Fount\r\n
+            Server: mio webserver\r\n\r\n"
+            .to_string()
+            .into_bytes()),
+        _ => Err(failure::err_msg("Undefined status Code.")),
     }
 }
 
